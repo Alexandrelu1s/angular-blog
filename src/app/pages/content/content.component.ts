bf4a3ac0687998dd9766757999bc72db0b31f619
alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-10-06 21:38:21 -0300

    Pegando os dados dinamicamente

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -10,9 +11,15 @@ export class ContentComponent implements OnInit {
   photoCover:string ="https://th.bing.com/th/id/OIP.Ihu2B55S_YG9mwUQ5Uo1YwHaE8?pid=ImgDet&rs=1";
   contentTitle: string = "NOTICIA";
   contentDescription: string="NAO TEM NOTICIA NENHUMA";
-  constructor() { }
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      console.log(value.get("id"))
+    )
   }
 
 }
